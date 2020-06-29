849881309450f276e8ec302e3384a2fae4a053a5
Alexander Doroshenko <ale.doroshenko@gmail.com>
2020-06-29 08:12:35 +0200

    Add some boilerplate for form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,97 @@
 import React from "react";
+import {
+  Grommet,
+  Box,
+  Main,
+  Heading,
+  Select,
+  FormField,
+  TextInput,
+  Button,
+  Form,
+} from "grommet";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
+
+const options = [
+  { label: "rinkeby", value: "rinkeby" },
+  { label: "mainnet", value: "mainnet" },
+];
+
+const defaultValue = {
+  address: "",
+  network: "rinkeby",
+};
 
 function App() {
+  const [value, setValue] = React.useState(defaultValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grommet theme={theme}>
+      <Box
+        direction="row-responsive"
+        justify="center"
+        align="center"
+        pad="xlarge"
+        gap="medium"
+      >
+        <Box
+          width="large"
+          height="large"
+          background="white"
+          elevation="medium"
+          pad="medium"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box>
+            <Heading margin="none">Wallet</Heading>
+          </Box>
+
+          <Box margin={{ top: "medium" }}>
+            <Form
+              value={value}
+              onChange={(nextValue) => {
+                console.log("Nextvalue", nextValue);
+                setValue(nextValue);
+              }}
+              onReset={() => setValue(defaultValue)}
+              onSubmit={({ value }: any) => {
+                console.log(value);
+              }}
+            >
+              <FormField
+                name="address"
+                htmlFor="address-id"
+                label="Address"
+                validate={{ regexp: /^[0-9]{4,6}$/, message: "4-6 digits" }}
+              >
+                <TextInput id="address-id" name="address" />
+              </FormField>
+              <FormField label="Network" name="network">
+                <Select
+                  name="network"
+                  options={options}
+                  labelKey="label"
+                  valueKey="value"
+                  value={options[0]}
+                />
+              </FormField>
+
+              <Box direction="row" justify="between" margin={{ top: "medium" }}>
+                <Button type="submit" label="Search" primary />
+              </Box>
+            </Form>
+          </Box>
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
 
