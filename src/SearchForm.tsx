17fc2b6cbebbcc061d113f19f5ad6188cb2b1b4a
Alexander Doroshenko <ale.doroshenko@gmail.com>
2020-07-01 11:40:32 +0200

    Add button spinner

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Form, FormField, Select, TextInput } from "grommet";
+import { Box, Button, Form, FormField, Select, TextInput, Text } from "grommet";
 import ethereumRegex from "ethereum-regex";
 import React from "react";
-import { useAppDispatch, TDispatch, TNetwork } from "./reducer";
+import { useAppDispatch, TDispatch, TNetwork, useAppState } from "./reducer";
 import { requestAccountData } from "./actions";
 
 const objectOptions = [
@@ -11,7 +11,6 @@ const objectOptions = [
 
 function validateAddress(address: string) {
   const isValidAddress = ethereumRegex({ exact: true }).test(address);
-
   if (!isValidAddress) {
     return "Invalid address";
   }
@@ -24,9 +23,41 @@ type TSearchFormProps = {
   };
 };
 
+const spinning = (
+  <svg
+    version="1.1"
+    viewBox="0 0 32 32"
+    width="24px"
+    height="24px"
+    fill="#FFFFFF"
+  >
+    <path
+      opacity=".25"
+      d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"
+    />
+    <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z">
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        from="0 16 16"
+        to="360 16 16"
+        dur="0.8s"
+        repeatCount="indefinite"
+      />
+    </path>
+  </svg>
+);
+
+export const Spinner = () => (
+  <Box align="center" justify="center">
+    {spinning}
+  </Box>
+);
+
 export function SearchForm({ currentFormData }: TSearchFormProps) {
   const [value, setValue] = React.useState(currentFormData);
   const dispatch = useAppDispatch();
+  const { isFetching } = useAppState();
 
   React.useEffect(() => {
     setValue(currentFormData);
@@ -72,7 +103,20 @@ export function SearchForm({ currentFormData }: TSearchFormProps) {
         </FormField>
 
         <Box direction="row" justify="between" margin={{ top: "medium" }}>
-          <Button type="submit" label="Search" primary />
+          <Button
+            type="submit"
+            label={
+              isFetching ? (
+                <Box direction="row" gap="small">
+                  <Spinner />
+                  <Text size="medium"> Loading... </Text>
+                </Box>
+              ) : (
+                "Search"
+              )
+            }
+            primary
+          />
         </Box>
       </Form>
     </React.Fragment>
